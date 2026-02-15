2c15b19ba641522e280d24805804be9d9f43f2e4
limzhilit <g00440629@atu.ie>
2026-02-15 22:08:32 +0000

    init

---
+++ src/main/java/com/zelig/oopsem2week3/OopSem2Week3Application.java
@@ -0,0 +1,13 @@
+package com.zelig.oopsem2week3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopSem2Week3Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OopSem2Week3Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/zelig/oopsem2week3/OopSem2Week3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zelig.oopsem2week3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopSem2Week3ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
